2927a2f865ef9fefcaf0bea8ff0be3cda3cb1fe0
VickyaWei <vickyaa.wei@gmail.com>
2024-06-25 13:26:04 -0700

    Initial commit

---
+++ kitchen/src/App.js
@@ -0,0 +1,12 @@
+import Carousel from "./components/Carousel/Carousel";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
